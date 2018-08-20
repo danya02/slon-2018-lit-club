e76736064a02799db2eb78243ec81ef4a615212f
Danya Generalov <danya02@me.com>
2018-08-20 20:31:19 +0300

    Adding an alternate-language build option.

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 all: text show
+ru: textru showru
 
 text:
 	python3 build.py
+textru:
+	python3 build.py ru
 
 show:
 	cat output.txt
+showru:
+	cat output.ru.txt
 
 clean:
 	rm -f output.txt
--- build.py
+++ build.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+import sys
+try:
+    lang = sys.argv[1]
+except IndexError:
+    lang = None
+
 import os
 text = '{main}'
 while '{' in text:
@@ -7,20 +13,39 @@ while '{' in text:
     before,sep,after = text.partition('{')
     topic,sep,after = after.partition('}')
     topic+='.txt'
-    if os.path.sep in topic:
+    if os.path.sep in (lang+os.path.sep+topic if lang else topic):
         try:
-            os.makedirs(os.path.dirname(topic))
+            os.makedirs(os.path.dirname((lang+os.path.sep+topic if lang else topic)))
         except FileExistsError:
             pass
-    try:
-        with open(topic) as o:
-            inset = o.read()
-    except FileNotFoundError:
-        inset = '####{}####'.format(topic)
-        with open(topic,'w') as o:
-            o.write(inset)
+        except FileNotFoundError:
+            pass
+    if not lang:
+        try:
+            with open(topic) as o:
+                inset = o.read()
+        except FileNotFoundError:
+            inset = '####{}####'.format(topic)
+            with open(topic,'w') as o:
+                o.write(inset)
+    else:
+        try:
+            with open(lang+os.path.sep+topic) as o:
+                inset = o.read()
+        except FileNotFoundError:
+            try:
+                with open(topic) as o:
+                    inset = o.read()
+            except FileNotFoundError:
+                inset = '####{}####'.format(topic)
+                with open(topic,'w') as o:
+                    o.write(inset)
+            inset+=f'({lang+os.path.sep+topic})'
+            with open(lang+os.path.sep+topic,'w') as o:
+                o.write(inset)
+
     if inset[-1]=='\n':
         inset = inset[:-1]
     text = before+inset+after
-with open('output.txt','w') as o:
+with open('output'+(f'.{lang}' if lang else '')+'.txt','w') as o:
     o.write(text)
